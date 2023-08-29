cfaf044c870abaa20b7d24921220c3c086afecde
Gabriel <gabrielmenezes_@hotmail.com>
2023-08-29 09:22:07 -0400

    adicionando endpoints (get e post)

--- index.js
+++ index.js
@@ -10,5 +10,18 @@ app.get("/oi", function (req, res) {
     res.send("Hello World do oi");
   });
 
- 
+  //endspoints de herois
+  const lista = ["Mulher Maravilha, Capitã Marvel", "Homem de ferro"]
+  
+  //Real All -> [GET] /herois
+  app.get("/herois", function(req, res) { // posso usar tbm o _ no lugar no req já que não estou usando
+    res.send(lista);
+  }); //declaração básica do endpoint
+
+
+  //Create -> [POST] /herois
+  app.post("/herois",function(req,res){
+    res.send("criar registro");
+  });
+
 app.listen(3000);
