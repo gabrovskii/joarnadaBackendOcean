20b0981891f9eee962a7ae99747d692afabd9376
Gabriel <gabrielmenezes_@hotmail.com>
2023-08-30 10:10:24 -0400

    inserindo conexão com mongoDB

--- index.js
+++ index.js
@@ -1,48 +1,102 @@
 const express = require("express");
+const { MongoClient } = require("mongodb");
+const url = "mongodb://localhost:27017";
+const dbName = "jornada-backend";
+const client = new MongoClient(url);
+
+
+async function main(){
+console.info("conectando ao banco");
+await client.connect();
+console.info("Banco de dados conectado");
+const db = client.db(dbName);
+const collection = db.collection("herois");
+
+
+
+
+
+
+
+
+
 const app = express();
 
-//habilitamos o processasmento de JSON
+// Habilitamos o processamento de JSON
 app.use(express.json());
 
-
+// Endpoint Principal
 app.get("/", function (req, res) {
   res.send("Hello World");
 });
 
+// Endpoint /oi
 app.get("/oi", function (req, res) {
-    res.send("Hello World do oi");
-  });
+  res.send("Olá, mundo!");
+});
 
-  //endspoints de herois
-  const lista = ["Mulher Maravilha, Capitã Marvel", "Homem de ferro"]
-  
-  //Real All -> [GET] /herois
-  app.get("/herois", function(req, res) { // posso usar tbm o _ no lugar no req já que não estou usando
-    res.send(lista);
-  }); //declaração básica do endpoint
+// Endpoints de Herois
+const lista = ["Mulher Maravilha", "Capitã Marvel", "Homem de Ferro"];
+//             0                    1                2
 
+// Read All -> [GET] /herois
+app.get("/herois", function (req, res) {
+  res.send(lista.filter(Boolean));
+});
 
-  //Create -> [POST] /herois
-  app.post("/herois",function(req,res){
-    //console.log(req.body)
+// Create -> [POST] /herois
+app.post("/herois", function (req, res) {
+  // console.log(req.body, typeof req.body);
 
-    //extrair o nome do Body da request (corpo da requisição)
-    const item = req.body.nome;
+  // Extrai o nome do Body da Request (Corpo da Requisição)
+  const item = req.body.nome;
 
-    //insere o item na lista
-    lista.push(item)
+  // Inserir o item na lista
+  lista.push(item);
 
-    res.send("item criado com sucesso");
-  });
+  // Enviamos uma resposta de sucesso
+  res.send("Item criado com sucesso!");
+});
 
-  //Read by Id -> [GET] /herois/:id
-  app.get("/herois/:id", function(req,res){
-    //pegamos o parametro de rota ID
-    const id = req.params.id -1;
-    //pegamos a informação da lista
-    const item = lista[id];
-    //exibimos o item na resposta do endpoint
-    res.send(item);
-  });
+// Read By Id -> [GET] /herois/:id
+app.get("/herois/:id", function (req, res) {
+  // Pegamos o parâmetro de rota ID
+  const id = req.params.id - 1;
+
+  // Pegamos a informação da lista
+  const item = lista[id];
+
+  // Exibimos o item na resposta do endpoint
+  res.send(item);
+});
+
+// Update -> [PUT] /herois/:id
+app.put("/herois/:id", function (req, res) {
+  // Pegamos o parâmetro de rota ID
+  const id = req.params.id - 1;
+
+  // Extrai o nome do Body da Request (Corpo da Requisição)
+  const item = req.body.nome;
+
+  // Atualizamos a informação na lista de registros
+  lista[id] = item;
+
+  res.send("Item editado com sucesso!");
+});
+
+// Delete -> [DELETE] /herois/:id
+app.delete("/herois/:id", function (req, res) {
+  // Pegamos o parâmetro de rota ID
+  const id = req.params.id - 1;
+
+  // Excluir o item da lista
+  delete lista[id];
+
+  res.send("Item excluído com sucesso!");
+});
 
 app.listen(3000);
+}
+
+
+main();
