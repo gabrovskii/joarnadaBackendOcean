e7a48e06dde1a777600d1edc312c91a2cf4e67fa
Gabriel <gabrielmenezes_@hotmail.com>
2023-08-30 10:11:14 -0400

    criando arquivo conections para fins de melhor organização

--- conection.js
+++ conection.js
@@ -4,5 +4,11 @@ const db = "jornada-backend";
 const client = new MongoClient(url);
 
 
-async function main(){}
+async function main(){
+console.info("conectando ao banco");
+await client.connect();
+console.info("Banco de dados conectado");
+const db = client.db(dbName);
+const collection = db.collection("herois");
+}
 main();
